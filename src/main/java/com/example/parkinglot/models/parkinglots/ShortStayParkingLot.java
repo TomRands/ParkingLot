67700aac6f4f782a1e00ca065e016a6a1987fb5a
Ayush <pandeyayushraj4@gmail.com>
2023-09-28 14:20:04 +0100

    Short Term Stay is FIxed.

--- src/main/java/com/example/parkinglot/models/parkinglots/ShortStayParkingLot.java
+++ src/main/java/com/example/parkinglot/models/parkinglots/ShortStayParkingLot.java
@@ -6,20 +6,24 @@ import com.example.parkinglot.models.TimeSpan;
 public class ShortStayParkingLot implements IParkingLot{
 
     private final double MINUTE_RATE = 0.10;
+    private final double HOURLY_RATE = 6;
     private final int MAXIMUM_DAILY_RATE = 18;
     @Override
     public long calculateFee(Ticket ticket) {
-        double fee = 0;
+        double fee=0;
         TimeSpan duration = ticket.duration;
 
-        fee = duration.minutes * MINUTE_RATE;
-
-        //Maximum Daily Rate for Short Stays is 18 and anything over 3 hours is counted as Days.
-        if(duration.minutes > 180) {
+        if(duration.hours == 0) {
+            fee = duration.minutes * MINUTE_RATE;
+        } else if(duration.hours<=3) {
+            fee = (duration.hours * HOURLY_RATE) + duration.minutes * MINUTE_RATE;
+        }
+        else if(duration.hours > 3) {
             duration.days++;
             fee = duration.days * MAXIMUM_DAILY_RATE;
+        }else if (duration.days > 1){
+            fee = duration.days * MAXIMUM_DAILY_RATE;
         }
-
         fee = Math.ceil(fee);
 
         return (long) fee;
