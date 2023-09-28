245c084f01350488f8c9c1db666132dfd42ffa1b
Ayush <pandeyayushraj4@gmail.com>
2023-09-28 16:24:38 +0100

    Short Term Stay is FIxed. (AGAIN!)

--- src/main/java/com/example/parkinglot/models/parkinglots/ShortStayParkingLot.java
+++ src/main/java/com/example/parkinglot/models/parkinglots/ShortStayParkingLot.java
@@ -3,30 +3,25 @@ package com.example.parkinglot.models.parkinglots;
 import com.example.parkinglot.models.Ticket;
 import com.example.parkinglot.models.TimeSpan;
 
-public class ShortStayParkingLot implements IParkingLot{
+public class ShortStayParkingLot implements IParkingLot {
 
     private final double MINUTE_RATE = 0.10;
     private final double HOURLY_RATE = 6;
-    private final int MAXIMUM_DAILY_RATE = 18;
+    private final int MAXIMUM_DAILY_RATE = 36;
+
     @Override
     public long calculateFee(Ticket ticket) {
-        double fee=0;
+        double fee = 0;
         TimeSpan duration = ticket.duration;
 
-        if(duration.hours == 0) {
-            fee = duration.minutes * MINUTE_RATE;
-        } else if(duration.hours<=3) {
-            fee = (duration.hours * HOURLY_RATE) + duration.minutes * MINUTE_RATE;
-        }
-        else if(duration.hours > 3) {
+        //Checking if hours is more than 3.
+        if(duration.hours>6){
             duration.days++;
-            fee = duration.days * MAXIMUM_DAILY_RATE;
-        }else if (duration.days > 1){
-            fee = duration.days * MAXIMUM_DAILY_RATE;
+            duration.hours=0;
         }
+        fee = duration.hours*HOURLY_RATE + duration.days * MAXIMUM_DAILY_RATE + duration.minutes * MINUTE_RATE;
         fee = Math.ceil(fee);
 
         return (long) fee;
-
     }
 }
